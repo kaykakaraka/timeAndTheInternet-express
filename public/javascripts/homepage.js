3c46104a2d36695be7c535d65901e953e2f07616
kaykakaraka <23278323+kaykakaraka@users.noreply.github.com>
2023-08-19 12:34:21 +0100

    Clicking a button changes the visible title. Script currently in the index.jade page

--- public/javascripts/homepage.js
+++ public/javascripts/homepage.js
@@ -1,11 +1,67 @@
-class Title {
-  contructor() {
 
-  }
+const titles = [
+  'Time and the Internet',
+  'Sandra has a girlfriend',
+  'How To Go Outside',
+  'Sudoku'
+];
 
-  changeTitle() {
+let count = 0
+let title = titles[count]
 
-  }
-}
+const uiTitle = document.querySelector('h1');
+    const button = document.querySelector('button');
+    button.addEventListener('click', (event) => {
+      event.preventDefault();
+      count += 1;
+      uiTitle.value = titles[count];
+    });
 
-module.exports = Title;
+
+// class Title {
+//   contructor() {
+//     this.titles = [
+//       'Time and the Internet',
+//       'Sandra has a girlfriend',
+//       'How To Go Outside',
+//       'Sudoku'
+//     ];
+//     this.count = 0;
+//     this.title = this.titles[this.count];
+//     const uiTitle = document.querySelector(h1);
+//     const button = document.querySelector(button);
+//     button.addEventListener('click', (event) => {
+//       event.preventDefault();
+//       this.changeTitle();
+//       uiTitle.value = this.title;
+//     })
+//   }
+
+//   changeTitle() {
+//     this.count += 1;
+//     this.title = this.titles[this.count]
+//   }
+// }
+
+// const TitleGenerator = new Title;
+// module.exports = TitleGenerator;
+
+// const form = document.querySelector('form');
+//       const button = form.querySelector('button');
+//       const dreamsInput = document.querySelector('#dreamsInput');
+//       const lettingGoInput = document.querySelector('#lettingGoInput')
+//       const dreamsDiv = document.querySelector('#dreams')
+//       const lettingGoDiv = document.querySelector('#lettingGo')
+
+//       button.addEventListener('click', (event) => {
+//           event.preventDefault();
+//           const p = document.createElement('p');
+//           p.textContent = dreamsInput.value;
+//           dreamsInput.value = ''
+//           dreamsDiv.appendChild(p);
+//           const lettingGo = document.createElement('p');
+//           lettingGo.textContent = lettingGoInput.value;
+//           lettingGoInput.value = ''
+//           lettingGoDiv.appendChild(lettingGo);
+//       }); 
+
